8061fbd5d18d93f03d8430e6f5fb729532727a6e
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-08-22 20:23:06 +0800

    getting the bean using the class and also tested duplicate bean name

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpringJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpringJava.java
@@ -15,5 +15,10 @@ public class App02HelloWorldSpringJava {
 
         // 3. Retrieving Beans managed by Spring
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address2"));
+
+        System.out.println(context.getBean(Address.class));
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,35 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {};
+record Address (String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "John Daniel";
+    }
+
+    @Bean
+    public int age() {
+        return 22;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Stephanie  Joy", 22);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+
+    // When there is a duplicate name of a bean, the first found bean is returned
+    @Bean(name = "address2")
+    public Address address3() {
+        return new Address("Villa Palao", "Philippines");
     }
 }
