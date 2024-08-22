5f5667451bcff435830212b5819d185c5f7e4cc1
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-08-22 19:54:25 +0800

    created a spring configuration and context

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpringJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpringJava.java
@@ -1,7 +1,14 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App02HelloWorldSpringJava {
     public static void main(String[] args) {
 
+        // 1. Launch a Spring Context -
+
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        // 2. Configure the things that we want Spring to manage - @Configuration
     }
 }
