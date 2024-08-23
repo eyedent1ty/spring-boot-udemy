eb348efc5b984eb5087957ae68a0219ef6a1076a
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-08-23 23:54:34 +0800

    removed .idea folder

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -3,6 +3,7 @@ package com.in28minutes.learn_spring_framework.examples.a1;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
