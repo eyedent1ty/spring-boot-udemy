240568e1e30c921343c4bdc2181748c8dc6c3f12
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-08-23 23:29:31 +0800

    setup new packages to test different types of dependency injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -1,11 +1,10 @@
-package com.in28minutes.learn_spring_framework;
-
-import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.GamingConsole;
+package com.in28minutes.learn_spring_framework.game;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 @ComponentScan("com.in28minutes.learn_spring_framework.game")
@@ -13,7 +12,7 @@ public class GamingAppLauncherApplication {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
                 GamingAppLauncherApplication.class)) {
-            System.out.println(context.getBean(GamingConsole.class));
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
         }
     }
 }
