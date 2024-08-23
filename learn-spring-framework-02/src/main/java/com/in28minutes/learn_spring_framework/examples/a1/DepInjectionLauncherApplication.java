66b85b30716a800457d3eb3da02b81db37f8c8ae
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-08-23 23:42:46 +0800

    implemented the constructor dependency injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -11,22 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 class YourBusinessClass {
     Dependency1 dependency1;
-
     Dependency2 dependency2;
+
+    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor is called automatically - Autowired the depedencies");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
 
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2;
-    }
-
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setters used");
-        this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        this.dependency2 = dependency2;
     }
 }
 
